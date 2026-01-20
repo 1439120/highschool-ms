874ddbe294bf73dd946e1e2cff6b9143afd30dc3
Nkosana <nkosana.mabuza2@gmail.com>
2026-01-20 16:47:32 +0200

    making dynamic placeholder for searchbar

--- front-end/highschool-ms/src/app/components/searchbar/searchbar.ts
+++ front-end/highschool-ms/src/app/components/searchbar/searchbar.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -11,6 +11,13 @@ export class Searchbar {
   @Input() searchQuery : string = '';
 
   @Output() onSearch = new EventEmitter<string>();
+
+  searchItems = input<string[]>([]);
+  tableTitle = input('')
+
+  searchByPlaceholder = computed(() => {
+    return `Search ${this.tableTitle().toLocaleLowerCase()} by ${this.searchItems().slice(0,3).join(',').replaceAll('_',' ').replaceAll(',',', ')}...`
+  })
 
   onChangeSearch(){
     this.onSearch.emit(this.searchQuery);

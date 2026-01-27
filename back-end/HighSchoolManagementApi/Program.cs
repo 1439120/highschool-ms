b4aeb2273c801517cbc305e289bc94e97812d701
Nkosana <nkosana.mabuza2@gmail.com>
2026-01-27 09:58:19 +0200

    Changing from openAi to swagger

--- back-end/HighSchoolManagementApi/Program.cs
+++ back-end/HighSchoolManagementApi/Program.cs
@@ -1,18 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+// 🔹 Sample endpoint
+app.MapGet("/api/hello", () => "Hello World");
 
 var summaries = new[]
 {

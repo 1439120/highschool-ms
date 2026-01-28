bb753104052113d391aef1bff916fdb316b62fe6
Nkosana <nkosana.mabuza2@gmail.com>
2026-01-28 14:03:24 +0200

    adding the update and delte of users

--- back-end/HighSchoolManagementApi/Controllers/UsersController.cs
+++ back-end/HighSchoolManagementApi/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using HighSchoolManagementApi.Data;
 using HighSchoolManagementApi.Mappers;
 using HighSchoolManagementApi.Dtos.Users;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HighSchoolManagementApi.Controllers
 {
@@ -37,9 +38,44 @@ namespace HighSchoolManagementApi.Controllers
         public IActionResult Create([FromBody] CreateUsersRequestDto usersDto)
         {
             var usersModel = usersDto.ToUsersFromCreateDTO();
-            _context.Users.Add(usersModel);
-            _context.SaveChanges();
+            _context.Users.Add(usersModel); // tracking
+            _context.SaveChanges(); // now it is sent to the server
             return CreatedAtAction(nameof(GetById), new {id = usersModel.Id}, usersModel.ToUsersDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateUsersRequestDto updateDto)
+        {
+            var usersModel = _context.Users.FirstOrDefault(x => x.Id == id);
+            if(usersModel == null) return NotFound();
+
+            usersModel.Name = updateDto.Name;
+            usersModel.Surname = updateDto.Surname;
+            usersModel.Phone = updateDto.Phone;
+            usersModel.Email = updateDto.Email;
+            usersModel.Role = updateDto.Role;
+            usersModel.Address = updateDto.Address;
+            usersModel.DateOfBirth = updateDto.DateOfBirth;
+            usersModel.DateJoined = updateDto.DateJoined;
+            usersModel.Type = updateDto.Type;
+            usersModel.CreatedOn = updateDto.CreatedOn;
+
+            _context.SaveChanges();
+            return Ok(usersModel.ToUsersDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var usersModel = _context.Users.FirstOrDefault(x => x.Id == id);
+            if(usersModel == null) return NotFound();
+
+            _context.Users.Remove(usersModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
     }
 }
